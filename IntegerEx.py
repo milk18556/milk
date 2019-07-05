f08b6fa94e2b4a9c962c257e342ef420fc4e58f5
milk18556 <52314720+milk18556@users.noreply.github.com>
2019-07-05 16:24:27 +0700

    Add files via upload
    Integer

---
+++ IntegerEx.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(18/2)
+print(18+2)
+print(18-2)
+print(18*2)
+
+
+# In[2]:
+
+
+print(type(18/2))
+print(type(18+2))
+print(type(18-2))
+print(type(18*2))
+
+
+# In[ ]:
+
+
+
+
