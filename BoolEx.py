5b7a9498f41fb6d90731daeb5b5ac39b887be7e1
milk18556 <52314720+milk18556@users.noreply.github.com>
2019-07-05 16:27:14 +0700

    Add files via upload
    Bool

---
+++ BoolEx.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print(type(True))
+print(True)
+print(False)
+
+
+# In[3]:
+
+
+print(True and False)
+
+
+# In[4]:
+
+
+print(True or False)
+
+
+# In[ ]:
+
+
+
+
