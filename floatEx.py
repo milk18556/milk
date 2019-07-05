c6bb0f871e75d7aa8d040616fb71a2ed9795e9c9
milk18556 <52314720+milk18556@users.noreply.github.com>
2019-07-05 16:25:44 +0700

    Add files via upload
    Float

---
+++ floatEx.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(10+3.14)
+
+
+# In[2]:
+
+
+print(10+3.10)
+
+
+# In[3]:
+
+
+print(10+3.0)
+
+
+# In[4]:
+
+
+print(10-3.0)
+
+
+# In[5]:
+
+
+print(5*1.0)
+
+
+# In[6]:
+
+
+print(15.1*2)
+
+
+# In[ ]:
+
+
+
+
