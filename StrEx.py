19ea3fdaaa87e99f70d298336e40de665c9d82fd
milk18556 <52314720+milk18556@users.noreply.github.com>
2019-07-05 16:28:01 +0700

    Add files via upload
    String

---
+++ StrEx.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello")
+print(type("Hello"))
+print("Hello"+"World")
+
+
+# In[3]:
+
+
+print("Hello" - "World")
+
+
+# In[6]:
+
+
+print("Hello"*5)
+
+
+# In[7]:
+
+
+print("Hello"/5)
+
+
+# In[ ]:
+
+
+
+
