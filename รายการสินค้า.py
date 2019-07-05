b781e0f9d52d45367d483bafd7f3d4036a922e58
milk18556 <52314720+milk18556@users.noreply.github.com>
2019-07-05 16:36:03 +0700

    Add files via upload
    รายการสินค้า

---
+++ รายการสินค้า.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("Milk Shop")
+print("----------------------------------------")
+print("1.Water x 1 10 THB = 10THB")
+print("2.Lay  x 3 10 THB    = 30THB")
+print("3.Icecream x 3 10 THB = 30THB")
+
+
+# In[ ]:
+
+
+
+
