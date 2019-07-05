92338b73d7e4d84c0ecd4a0f04047056209b60cb
milk18556 <52314720+milk18556@users.noreply.github.com>
2019-07-05 16:20:32 +0700

    Add files via upload
    ใบเสร็จ

---
+++ receipt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+print("CP ALL, 7-Eleven ราชมงคลสุวรรณภูมิ(สา(06582) ")
+print("TAX#0107542000011 (VAT Included)")
+print("Vat Code 04968 POS#E06720000201243")
+print("ใบเสร็จรับเงิน/ใบกำกับภาษีอย่างย่อ")
+print("1 S-Gulp โคล่า                 12.00 THB")
+print("1 อิชิตันกรีนที ต้นตำรับ       20.00 THB")
+print("ยอดสุทธิ 2 ชิ้น                 32.00 THB")
+print("เงินสด/เงินทอน 100.00  68.00")
+
+
+# In[ ]:
+
+
+
+
