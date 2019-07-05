d0286585511237c3d3b1b213fd3dcfc699eb6be7
milk18556 <52314720+milk18556@users.noreply.github.com>
2019-07-05 16:29:09 +0700

    Add files via upload
    Datatype

---
+++ Datatype.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("Welcome to Milk Shop")
+print("---------------------------")
+print("Banana   2 15    THB" )
+print("Testo      2 20    THB")
+print("Water      2 10   THB")
+
+
+# In[5]:
+
+
+print("Welcome to Milk Shop")
+print("---------------------------")
+print("Banana",2 ,15, "THB", 2*15, "THB" )
+print("Testo",2 ,20, "THB", 2*20, "THB" )
+print("Water",2 ,10, "THB", 2*10, "THB" )
+print("Total",30+40+40, "THB" )
+
+
+# In[ ]:
+
+
+
+
