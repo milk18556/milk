ab57b6ded8296be42cbd215704bb1587955ea1b5
milk18556 <52314720+milk18556@users.noreply.github.com>
2019-07-05 14:07:16 +0700

    Add files via upload
    milk

---
+++ milk.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print(type(10))
+
+
+# In[5]:
+
+
+print(type("Hollo"))
+
+
+# In[6]:
+
+
+print(type(True))
+
+
+# In[7]:
+
+
+print(type(10.3))
+
+
+# In[8]:
+
+
+print(type(1j))
+
+
+# In[9]:
+
+
+print((10+3.5))
+
+
+# In[ ]:
+
+
+
+
