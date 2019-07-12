3051cb47a87a5a2d659c8d19c28ba8d7109a67f9
milk18556 <52314720+milk18556@users.noreply.github.com>
2019-07-12 14:16:43 +0700

    Add files via upload

---
+++ SubTest1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+print("#1.น.ส.กวินนา พลเสน 362515241001 EE362541N")
+print("#2.น.ส.นีรชา ชุมมิน 362515241007 EE36241N")
+
+
+# In[17]:
+
+
+print("R1=100 Ohm.")
+print("R2=300 Ohm.")
+print("RT=(R1*R2)/(R1+R2)")
+print("RT=75 Ohm.")
+
+
+# In[ ]:
+
+
+
+
