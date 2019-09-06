64ed5be2319315c32df86aa6a549c0243825d2fe
milk18556 <52314720+milk18556@users.noreply.github.com>
2019-09-06 22:42:43 +0700

    Add files via upload

---
+++ Triangle3 (1).py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# นางสาวกวินา  พลเสน 362515241001 EE36241N
+max = 0
+i = 3
+sum = 0
+for i in range(i):
+    if angle > max:
+        max = angle
+    angle = int(input("enter angle :"))
+    sum = sum + angle
+print("sum :", sum)
+print("hypotenuse", max)
+
